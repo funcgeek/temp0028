7acc038f36e0d4818f71ca5693293ac96afb7d0c
Eric Iheanyichukwu <chisomezenna@gmail.com>
2025-09-16 17:30:00 +0100

    Sed
    sdds

--- application/modules/finance/controllers/Finance.php
+++ application/modules/finance/controllers/Finance.php
@@ -1218,7 +1218,7 @@ $arr = explode(',',$string);
 
     //Load customInvoice View Page
 
-    $this->load->vew('home/dashboard');
+    $this->load->view('home/dashboard');
     $this->load->view('custom_invoice', $data);
     $this->load->view('home/footer');
     }
