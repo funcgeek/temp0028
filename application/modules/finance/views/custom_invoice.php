e4e7f06d188674d2fe26242d6cc9a18086b3a086
Eric Iheanyichukwu <chisomezenna@gmail.com>
2025-09-17 09:18:31 +0100

    sedd

--- application/modules/finance/views/custom_invoice.php
+++ application/modules/finance/views/custom_invoice.php
@@ -3,7 +3,7 @@
         <section class="patient_report_container">
             <header class="panel-heading patient_report_header">
                 <strong>Custom Invoice</strong>
-                   <p class="patient_report_clause">Custom Invoices for <?php $_GET['patient'];?></p>
+                   <p class="patient_report_clause">Custom Invoices for <?php echo $_GET['patient'];?></p>
             </header>
 
 </section>
