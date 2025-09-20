77f8a13a8903679c31108c3cb097f286a10c0f4a
Eric Iheanyichukwu <chisomezenna@gmail.com>
2025-09-20 08:22:12 +0100

    eft
    ttyyuu

--- application/modules/prescription/views/glass_prescriptions_list.php
+++ application/modules/prescription/views/glass_prescriptions_list.php
@@ -31,8 +31,7 @@ $total_prescriptions = count($prescriptions);
 mysqli_close($conn);
 ?>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+   
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     <style>
